e20d9a30965d5059145406b02153e5dee69478bb
saikarun235 <81152134+saikarun235@users.noreply.github.com>
2021-05-12 04:53:49 -0400

    Add files via upload

---
+++ patient_values.sql
@@ -0,0 +1,30 @@
+INSERT INTO patient VALUES (1200,'sashank','valluri', 'india','m', '12-JAN-2021', '24-FEB-2021');
+INSERT INTO patient VALUES (1000,'sai','kumar', 'texas','m', '22-MAY-2020', '22-JUNE-2020');
+
+INSERT INTO patient VALUES (1300,'abhishek','kesoju', 'india','m', '15-JAN-2021', '28-FEB-2021');
+INSERT INTO patient VALUES (1100,'sreedhar','boina', 'columbus','m', '10-MAY-2020', '21-JUNE-2020');
+
+INSERT INTO patient VALUES (1500,'shravan','bangaroju', 'canada','m', '12-FEB-2021', '24-FEB-2021');
+INSERT INTO patient VALUES (2000,'bhanu','teja', 'texas','m', '01-MAY-2020', '31-MAY-2020');
+
+INSERT INTO patient VALUES (2100,'sathwik','mainam', 'africa','m', '06-AUG-2021', '24-DEC-2021');
+INSERT INTO patient VALUES (1600,'guna','kandukuri', 'canada','m', '17-SEP-2020', '22-OCTOBER-2020');
+
+INSERT INTO patient VALUES (1800,'mohan','vangala', 'new zealand','m', '16-MAR-2021', '31-APR-2021');
+INSERT INTO patient VALUES (1900,'shekar','kamala', 'australia','m', '25-JUNE-2020', '10-JULY-2020');
+
+INSERT INTO patient VALUES (2500,'bobby','patel', 'india','m', '11-JAN-2021', '22-FEB-2021');
+INSERT INTO patient VALUES (2400,'sonali','patel', 'texas','f', '23-MAY-2020', '05-JUNE-2020');
+
+INSERT INTO patient VALUES (3600,'tanuja','reddy', 'australia','f', '11-NOV-2021', '24-DEC-2021');
+INSERT INTO patient VALUES (3000,'charitha','yadav', 'new zealand','f', '15-MAY-2020', '19-JULU-2020');
+
+INSERT INTO patient VALUES (2800,'varshini','chowdary', 'india','f', '11-JULY-2021', '24-AUG-2021');
+INSERT INTO patient VALUES (2900,'latha','swarna', 'texas','f', '16-MAY-2020', '05-JUNE-2020');
+
+INSERT INTO patient VALUES (4100,'sam','jam', 'australia','f', '12-JULY-2021', '24-AUG-2021');
+INSERT INTO patient VALUES (3500,'sue','allen', 'new zealand','f', '10-APR-2020', '22-JUNE-2020');
+
+INSERT INTO patient VALUES (3100,'john','wick', 'india','m', '17-JAN-2021', '29-FEB-2021');
+INSERT INTO patient VALUES (3200,'patrik','joseph', 'london','m', '14-MAY-2020', '25-SEP-2020');
+
